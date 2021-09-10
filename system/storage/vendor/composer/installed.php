737f87853aeb0fc8247b6ab48de153eb425362ef
Stanislav93 <stas.stecenko@ukr.net>
2021-09-10 14:35:25 +0300

    start

---
+++ system/storage/vendor/composer/installed.php
@@ -0,0 +1,69 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'newtik/lowocstore3037urk',
+  ),
+  'versions' => 
+  array (
+    'newtik/lowocstore3037urk' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'scssphp/scssphp' => 
+    array (
+      'pretty_version' => '1.1.1',
+      'version' => '1.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '824e4cec10b2bfa88eec5dac23991cb9106622c1',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '46cd95797e9df938fdd2b03693b5fca5e64b01ce',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.23.1',
+      'version' => '1.23.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9174a3d80210dca8daa7f31fec659150bbeabfc6',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v2.14.6',
+      'version' => '2.14.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '27e5cf2b05e3744accf39d4c68a3235d9966d260',
+    ),
+    'zoujingli/wechat-php-sdk' => 
+    array (
+      'pretty_version' => 'v1.3.18',
+      'version' => '1.3.18.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd37d0c1919ede2ee54e65100ac3792e947b1e0ef',
+    ),
+  ),
+);
